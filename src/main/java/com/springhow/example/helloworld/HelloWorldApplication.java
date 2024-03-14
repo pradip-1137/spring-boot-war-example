0671a95399985166fb99336a4328587e98bb14a3
pradip-1137 <96612151+pradip-1137@users.noreply.github.com>
2024-03-14 11:48:16 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Hello World!";
+        return "Hello From Unnati Kucl...! After change in GitHub";
     }
 
 }
